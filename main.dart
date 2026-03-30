52171d905a995d63f7ce7208bdab92d8f7eab0e3
Ismail Shaer <ismailshaer3@gmail.com>
2026-03-30 23:31:06 +0300

    Modify title of course

--- main.dart
+++ main.dart
@@ -4,11 +4,14 @@ import 'observer.dart';
 
 void main(List<String> args) {
   Observer observer1 = Student("Ismail Shaer");
-  Observer observer2 = Student("Mohammed Hissy");
+  Observer observer2 = Student(" Amro Khaled");
+  Observer observer3 = Student(" hany hathout");
 
-  Course course = Course(name: "Mastering Flutter from zero to hero");
+  Course course = Course(name: " Ronaldo's routine life  ");
   course.subscribe(observer1);
   course.subscribe(observer2);
+  course.subscribe(observer3);
+
   course.unsbscribe(observer2);
 
   course.setAvailability(true);
