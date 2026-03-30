b213896b3a38a4c57630880f68bae7e46ed59166
Ismail Shaer <ismailshaer3@gmail.com>
2026-03-30 22:22:11 +0300

    update the name of course object and delete an observer object

--- main.dart
+++ main.dart
@@ -5,13 +5,11 @@ import 'observer.dart';
 void main(List<String> args) {
   Observer observer1 = Student("Ismail Shaer");
   Observer observer2 = Student("Mohammed Hissy");
-  Observer observer3 = Student("Karem Abu Shbak");
 
-  Course course1 = Course(name: "Mastering Flutter from zero to hero");
-  course1.subscribe(observer1);
-  course1.subscribe(observer2);
-  course1.subscribe(observer3);
-  course1.unsbscribe(observer2);
+  Course course = Course(name: "Mastering Flutter from zero to hero");
+  course.subscribe(observer1);
+  course.subscribe(observer2);
+  course.unsbscribe(observer2);
 
-  course1.setAvailability(true);
+  course.setAvailability(true);
 }
